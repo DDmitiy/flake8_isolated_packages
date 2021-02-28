2ae99bff8df7e7d7de42270030ed840e2cc81db8
d.dudov <YEBupwvV48>
2021-02-28 17:02:25 +0300

    Configure setups

--- setup.py
+++ setup.py
@@ -1,2 +1,37 @@
 from setuptools import setup
-setup()
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='flake8-isolated-packages',
+    packages=['./'],
+    version='0.1.1',
+    license='MIT',
+    description='This flake8 plugin is for checking imports isolations.',
+    long_description=long_description,
+    author='Dudov Dmitriy (ddmitiy)',
+    author_email='dudov.dm@gmail.com',
+    url='https://github.com/DDmitiy/flake8_isolated_packages',
+    keywords=['flake8', 'plugin', 'imports', 'packages', 'isolation'],
+    install_requires=[
+        'importlib-metadata',
+        'flake8',
+        'astpretty',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
